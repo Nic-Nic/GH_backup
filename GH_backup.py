0b8bcaac05b6cd0ca2051d3814a9cfdc2f5836d8
Nico <nicolaspalacio91@gmail.com>
2020-10-01 13:15:02 +0200

    Updated to Python3

--- GH_backup.py
+++ GH_backup.py
@@ -40,7 +40,7 @@ def request(url, multipage=True):
 
 if __name__ == '__main__':
     start = time.time()
-    print 'Session started @ %s\n' % time.strftime('%a %d %b %Y - %H:%M')
+    print('Session started @ %s\n' % time.strftime('%a %d %b %Y - %H:%M'))
 
     __user__ = sys.argv[1]
     __token__ = token(filename=sys.argv[2])
@@ -56,31 +56,31 @@ if __name__ == '__main__':
         name = repo['name']
 
         msg = 'Working on %s repository' % name
-        print msg
-        print '=' * len(msg) + '\n'
+        print(msg)
+        print('=' * len(msg) + '\n')
 
         if os.path.exists(name): # Repository already cloned
-            print '- Directory "%s/" already exists' % name
+            print('- Directory "%s/" already exists' % name)
 
             cloned = False
 
         else: # Clone the repository
-            print '- Directory "%s/" not found' % name
-            print '- Cloning repository "%s"' % name
+            print('- Directory "%s/" not found' % name)
+            print('- Cloning repository "%s"' % name)
 
             subprocess.call(('git clone --mirror %s %s/.git'
                              % (repo['ssh_url'], name)).split(' '),
                             stdout=sys.stdout)
             cloned = True
 
-        print '>>> Entering subdirectory "%s/"' % name
+        print('>>> Entering subdirectory "%s/"' % name)
         os.chdir(name)
 
         if cloned:
             subprocess.call('git config --bool core.bare false'.split(' '),
                             stdout=sys.stdout)
 
-        print '- Checking branches...'
+        print('- Checking branches...')
         # Check remote branches
         branch_url = repo['branches_url'].split('{')[0]
         aux = request(branch_url, multipage=False)
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
         if not cloned:
             # Check local branches
-            aux = subprocess.check_output('git branch'.split(' '))
+            aux = subprocess.check_output('git branch'.split(' '), encoding='utf-8')
             local = set([r.strip('* ') for r in aux.strip().split('\n')])
 
             common = remote & local
@@ -100,22 +100,22 @@ if __name__ == '__main__':
             #print 'COMMON:', common
 
             for branch in remote_only:
-                print '- Found new branch in remote: %s' % branch
+                print('- Found new branch in remote: %s' % branch)
                 subprocess.call(('git fetch origin %s' % branch).split(' '),
                                 stdout=sys.stdout)
 
             for branch in local_only:
-                print '- Local branch %s not in remote anymore' % branch
-                a = raw_input('Do you want to delete local branch %s?\n'
+                print('- Local branch %s not in remote anymore' % branch)
+                a = input('Do you want to delete local branch %s?\n'
                               % branch)
 
                 if a.lower()[0] == 'y':
-                    print '- Deleting branch %s' % branch
+                    print('- Deleting branch %s' % branch)
                     subprocess.call(('git branch -D %s' % branch).split(' '),
                                     stdout=sys.stdout)
 
                 else:
-                    print '- Branch %s is has been kept locally' % branch
+                    print('- Branch %s is has been kept locally' % branch)
 
         if cloned:
             common = remote
@@ -126,20 +126,20 @@ if __name__ == '__main__':
         common.append(u'master')
 
         for branch in common:
-            print '- Checking out branch %s' % branch
+            print('- Checking out branch %s' % branch)
             subprocess.call(('git checkout %s' % branch).split(' '),
                             stdout=sys.stdout)
             if not cloned:
-                print '- Pulling branch %s' % branch
+                print('- Pulling branch %s' % branch)
                 subprocess.call(('git pull origin %s' % branch).split(' '),
                                 stdout=sys.stdout)
 
-        print '<<< Leaving subdirectory "%s/"' % name
-        os.chdir('..')
-        print '\n'
+        print('<<< Leaving subdirectory "%s/"' % name)
+        os.chdir('../')
+        print('\n')
 
-    print 'Backed up %d repositories' % len(repos)
-    print 'Time elapsed %d sec.' %(time.time() - start)
+    print('Backed up %d repositories' % len(repos))
+    print('Time elapsed %d sec.' %(time.time() - start))
     msg = 'Backup finished!'
-    print '\n' + msg
-    print '=' * len(msg)
+    print('\n' + msg)
+    print('=' * len(msg))
